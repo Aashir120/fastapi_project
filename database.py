12bb41fec22c75465d5c8ce8a70f09a49a748feb
Aashir120 <60323401+Aashir120@users.noreply.github.com>
2025-04-02 00:52:36 +0500

    Update database.py

--- database.py
+++ database.py
@@ -4,7 +4,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # Database configuration
 # URL_DATABASE = 'mysql+pymysql://root:password@localhost:3306/flask'
-URL_DATABASE = 'mysql+pymysql://admin:password@database-1.cx64aigk03po.us-east-1.rds.amazonaws.com/flask'
 
 engine = create_engine(URL_DATABASE)
 
